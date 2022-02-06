bead30304c9113a60e82abb94d4f5e5325a1e75a
alex <alexander.jansen@studmail.w-hs.de>
2022-02-06 21:41:51 +0100

    Finished

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FPR_Pruefung_Aufgabe2
-FPR Pruefung Aufgabe2
+##Schlusswert
+Der Schlusswert erscheint nun erst zum Schluss, da auf jeden Thread die join() Methode angewandt wurde.
+Dies führt dazu, dass zuerst auf die Beendigung aller Threads gewartet wird, bis das Programm mit der Konsolenausgabe fortgesetzt wird.
+
+##Exception
+Ich habe die main-Methode in einen synchronized Kontext gesetzt. Dadurch wird die Datenkonsistenz gewährleistet, im Falle des 
+gleichzeitigen Zugriffs zweier Thread auf eine Variable.
--- src/MultithreadingAufgabe.java
+++ src/MultithreadingAufgabe.java
@@ -1,27 +1,43 @@
+import java.util.ArrayList;
+
 public class MultithreadingAufgabe {
-    public static void main(String[] args) {
-        class Counter {
-            private double c = 0.0;
-            void up() {
-                for (int i = 0; i < 100; i++)
-                    System.out.println(c++);
+
+    public static synchronized void main(String[] args) {
+
+        try {
+            class Counter {
+                private double c = 0.0;
+                void up() {
+                    for (int i = 0; i < 100; i++)
+                        System.out.println(c++);
+                }
+                void down() {
+                    for (int i = 0; i < 100; i++)
+                        System.out.println(c--);
+                }
+                double getC() {
+                    return c;
+                }
             }
-            void down() {
-                for (int i = 0; i < 100; i++)
-                    System.out.println(c--);
+
+            ArrayList<Thread> threadList = new ArrayList<>();
+            Counter count = new Counter();
+            for (int i = 0; i < 10; i++) {
+                Thread t = new Thread(() -> {
+                    count.up();
+                    count.down();
+                });
+                t.start();
+                threadList.add(t);
             }
-            double getC() {
-                return c;
+            for (Thread thread : threadList) {
+                thread.join();
             }
+            System.out.println("Schlusswert: " + count.getC());
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-        Counter count = new Counter();
-        for (int i = 0; i < 10; i++) {
-            Thread t = new Thread(() -> {
-                count.up();
-                count.down();
-            });
-            t.start();
-        }
-        System.out.println("Schlusswert: " + count.getC());
+
     }
 }
